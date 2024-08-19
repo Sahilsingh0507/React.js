5cdd93280dedfde30606955c238ea5077e8404cd
Sahil Singh <sahilkumar05072003@gmail.com>
2024-08-19 23:11:40 +0530

    react using create react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,18 @@
+// wo function ham yaha import karte hai us folder se 
+import Chai from "./Chai";
+
+function App() {
+  return (
+    // <h1>Chai Aur React</h1>
+    // jo function hamne import kiya usko ek tag me likh dete hai 
+
+    // ye basically div hi hai lekin ham isko yha gragment bolte hai 
+    <>
+      <Chai />
+      <h2>chai aur react.</h2>
+      <p>test para</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+function Chai() {
+    return (
+        <h2>Chai is ready.</h2>
+    );
+}
+
+// jo function banate hai usko export karte hai aur uska first letter capital hona compulsory hai 
+export default Chai;
