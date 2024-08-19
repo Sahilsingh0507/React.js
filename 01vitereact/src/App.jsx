4c615edf936f3f80f263ca4bdd633ddd4164f004
Sahil Singh <sahilkumar05072003@gmail.com>
2024-08-19 23:14:17 +0530

    react using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    // <h1>Chai aur React | Sahil Singh</h1>
+    // isme bss ek element hi return kar sakte hai 
+    // ye basically div jaisa hi hai par isko fragment bolte hai 
+    <>
+      <Chai />
+      <h1>chai aur react.</h1>
+      <p>test para</p>
+    </>
+  )
+}
+
+export default App
